fae6b35f006e1f834866cdaefeb7da186de810ee
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2023-06-08 18:09:44 +0300

    styles last slide

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -4,6 +4,8 @@ import fetch from './fetchMock';
 const feedbackInputs = Array.from(document.getElementsByName('feedback'));
 const contactSendBtn = document.querySelector('.contact-send');
 const finishScreen = document.querySelector('.finish');
+const quizWindow = document.querySelector('.quiz-window');
+const quizWindowInfo = document.querySelector('.quiz-window__info');
 
 contactSendBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -27,5 +29,7 @@ contactSendBtn.addEventListener('click', (event) => {
     }).then(() => {
         contactSendBtn.closest('.step').classList.add('hidden');
         finishScreen.classList.remove('hidden');
+        quizWindowInfo.classList.add('hidden');
+        quizWindow.style.maxWidth = '40rem';
     });
 });
